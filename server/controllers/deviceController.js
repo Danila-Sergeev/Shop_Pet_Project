24ccfa8f44401f4fa8a17cc419ff5dbdd886ca87
Sergeev Danila <Danilasergeev2003@icloud.com>
2024-02-27 19:25:36 +0300

    add functional to controllers

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,4 +1,4 @@
-const { Device } = require("../models/models");
+const { Device, DeviceInfo } = require("../models/models");
 const ApiError = require("../error/ApiError");
 const uuid = require("uuid");
 const path = require("path");
@@ -8,7 +8,9 @@ class DeviceController {
     try {
       const { name, price, brandId, typeId, info } = req.body;
       const { img } = req.files;
+      // задаем изображению уникальное название
       let fileName = uuid.v4() + ".jpg";
+      // Перемещаем изображения в папку static
       img.mv(path.resolve(__dirname, "..", "static", fileName));
 
       const device = await Device.create({
@@ -19,17 +21,36 @@ class DeviceController {
         img: fileName,
       });
 
+      // Заполняем талицу DeviceInfo
+      if (info) {
+        info = JSON.parse(info);
+        info.forEach((element) => {
+          DeviceInfo.create({
+            title: element.title,
+            description: element.description,
+            deviceId: device.id,
+          });
+        });
+      }
+
       return res.json(device);
     } catch (e) {
       next(ApiError.badRequest(e.message));
     }
   }
+
   async getAll(req, res) {
     let { brandId, typeId, limit, page } = req.query;
+
+    // Устанавливаем лимит по количеству отображаемых товаров на одной странице
+
     page = page || 1;
     limit = limit || 9;
     let offset = page * limit - limit;
     let devices;
+
+    // Реализуем сортировку по типу и бренду
+
     if (!brandId && !typeId) {
       devices = await Device.findAndCountAll({ limit, offset });
     }
@@ -56,7 +77,16 @@ class DeviceController {
     }
     return res.json(devices);
   }
-  async getOne(req, res) {}
+
+  async getOne(req, res) {
+    const { id } = req.params;
+    const device = await Device.findOne({
+      where: { id },
+      // Сразу подгружем DeviceInfo
+      include: [{ model: DeviceInfo, as: "info" }],
+    });
+    return res.json(device);
+  }
 }
 
 module.exports = new DeviceController();
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,14 +1,53 @@
 const ApiError = require("../error/ApiError");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { User, Basket } = require("../models/models");
+
+const generateJwt = (id, email, role) => {
+  return jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
+    expiresIn: "24h",
+  });
+};
 
 class UserController {
-  async registration(req, res) {}
-  async login(req, res) {}
-  async check(req, res, next) {
-    const { id } = req.query;
-    if (!id) {
-      return next(ApiError.badRequest("Не задан id"));
+  async registration(req, res) {
+    const { email, password, role } = req.body;
+    // проверяем данные на корректность
+    if (!email || !password) {
+      return next(ApiError.badRequest("Некорректный email или password"));
     }
-    res.json(id);
+    const candidate = await User.findOne({ where: { email } });
+    // проверям регистрировался ли пользователь ранее
+    if (candidate) {
+      return next(
+        ApiError.badRequest("Пользоатель с таким email уже существует")
+      );
+    }
+    //хешируем пароль, 5 - сколько раз будет хешировать
+    const hashPassword = await bcrypt.hash(password, 5);
+    const user = await User.create({ email, password: hashPassword, role });
+    const basket = await Basket.afterCreate({ userId: user.id });
+    const token = generateJwt(user.id, user.email, user.role);
+    return res.json({ token });
+  }
+  async login(req, res, next) {
+    const { email, password, role } = req.body;
+    // Ищем пользователя в БД
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return next(ApiError.internal("Пользователь не найден"));
+    }
+    // Сравниваем пароли
+    let comparePassword = bcrypt.compareSync(password, user.password);
+    if (!comparePassword) {
+      return next(ApiError.internal("Неверный пароль"));
+    }
+    const token = generateJwt(user.id, user.email, user.role);
+    return res.json({ token });
+  }
+  async check(req, res, next) {
+    const token = generateJwt(req.user.id, req.user.email, req.user.role);
+    return res.json({ token });
   }
 }
 
